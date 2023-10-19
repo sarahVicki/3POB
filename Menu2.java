f482f6902cc487d4fe280802d5a8b6bcbd58d2ad
Sarah Vanderei <142837608+sarahVicki@users.noreply.github.com>
2023-10-19 00:00:01 -0300

    Create Menu2.java

---
+++ Menu2.java
@@ -0,0 +1,13 @@
+
+//import java.time.LocalDate;
+//import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Menu menu = new Menu();
+    menu.Processamento();
+  }
+}
+
